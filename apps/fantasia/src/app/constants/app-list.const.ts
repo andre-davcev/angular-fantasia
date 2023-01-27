32e714f8bbd1f7fea65ba8f728a628a8d2acc741
Andre Davcev <>
2023-01-26 19:14:21 -0500

    Add alexandria club and remove memories

--- apps/fantasia/src/app/constants/app-list.const.ts
+++ apps/fantasia/src/app/constants/app-list.const.ts
@@ -2,8 +2,11 @@ import { AppProperties } from '../models';
 import { App } from '../enums';
 
 export const AppList: Array<AppProperties> = [
-  { key: App.Memories },
   { key: App.Firefly, link: 'https://firefly.im' },
+  {
+    key: App.Alexandria,
+    link: 'https://alexandriaclub.org',
+  },
   { key: App.Art, iconExtension: 'jpg' },
   { key: App.Movies, link: 'https://letterboxd.com/helaku/lists/by/name' },
 ];
--- apps/fantasia/src/app/enums/apps.enum.ts
+++ apps/fantasia/src/app/enums/apps.enum.ts
@@ -2,6 +2,7 @@ export enum App {
   Root = '',
   Home = 'home',
   Firefly = 'firefly',
+  Alexandria = 'alexandria',
   Memories = 'memories',
   Art = 'art',
   Movies = 'movies',
